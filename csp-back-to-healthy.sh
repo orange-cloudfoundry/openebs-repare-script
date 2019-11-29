054e94e4316c0542752bdde2eab8d8383df11ba6
olivier beyler <olivier.beyler@orange.com>
2019-11-29 11:25:36 +0100

    add check about running pod of pool

--- csp-back-to-healthy.sh
+++ csp-back-to-healthy.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #set -x
+kubectl get csp
 export state="Offline"
 export nb_csp_offline=$(kubectl get csp  |grep $state |wc -l)
 echo "nb csp "$state": "$nb_csp_offline
 if [ $nb_csp_offline = 2 ]
 then
+    # check if all 3 pod of  cstor-pool are in running mode
+    export nb_pod_cstor_pool=$(kubectl get pod -n openebs  -l app=cstor-pool |grep Running| wc -l)
+    if [ $nb_pod_cstor_pool = 3 ]
+    then 
         echo "csp "/${state}":"
         echo "============"
         csp_offline=$(kubectl get csp  |grep $state | cut -d ' ' -f1)
         ls_csp_offline=$(echo $csp_offline|sed 's/ /,/g')
         echo ${ls_csp_offline}
+        for u in csp_offline;
+          do
+            kubectl patch csp $u --type=merge --patch '{"status":{"phase":"Healthy"}}';
+          done;
         ## create the script for extrating the replica id of pvc impacted by csp
         export jsonpath="'{range .items[*]}{.metadata.name}{\" == \"}{.spec.replicaid}{\"\\\\n\"}'"
         echo "kubectl get cvr -n openebs -l 'cstorpool.openebs.io/name in ("${ls_csp_offline}")'  -o jsonpath=$jsonpath" > pvc_extract.sh
@@ -27,7 +36,10 @@ then
         echo "==============================================================="
         kubectl get  cStorVolume  -n openebs -o yaml |sed 's/consistencyFactor: 2/consistencyFactor: 1/g'|sed 's/replicationFactor: 3/replicationFactor: 1/g' > file.yaml
         for u in $(sh pvc_extract.sh | cut -d ' ' -f3); do cat file.yaml|sed 's/'$u'/#'${u}'/g' >tmp.yaml;mv tmp.yaml file.yaml; done
-        kubectl replace -f file.yaml --force
+        #       kubectl replace -f file.yaml --force
+    else
+     echo "pods of ctor pool are not all  in running mode => script not applicable"
+fi
 else
         echo "script not applicable"
 fi
